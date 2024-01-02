bfc6e2db7cbdb5b85958bb3e630e90c005291609
corporate insurance <151666354+cickenya@users.noreply.github.com>
2024-01-02 16:20:46 +0300

    Update production.py

--- production.py
+++ production.py
@@ -444,7 +444,7 @@ if uploaded_file is not None:
                 if isinstance(most_recent_date, pd.Series):
                     most_recent_date = most_recent_date.iloc[0]
                     
-                formatted_date = most_recent_longdate.strftime('%A %d %B %Y') 
+                formatted_date = most_recent_date.strftime('%A %d %B %Y') 
                 st.subheader(f'{formatted_date} PRODUCTION SUMMARY')
                 
                 with card_container(key="chart4"):
