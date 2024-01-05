0727510faf0abb53db556ae57615773e0a811e9a
corporate insurance <151666354+cickenya@users.noreply.github.com>
2024-01-05 09:27:30 +0300

    Update production.py

--- production.py
+++ production.py
@@ -118,11 +118,11 @@ if uploaded_file is not None:
         elif date_to_adjust.weekday() == 6:  # Sunday
             date_to_adjust -= timedelta(days=2)
 
-        yesterday_premium = yesterday + most_recent_date[most_recent_date['TRANSACTION DATE'] == date_to_adjust]['GROSS PREMIUM'].sum()
+        yesterday += most_recent_date[most_recent_date['TRANSACTION DATE'] == date_to_adjust]['GROSS PREMIUM'].sum()
 
     
     # yesterday_premium = most_recent_date['GROSS PREMIUM'].sum()
-    fom_yesterday_premium = "Ksh. {:,.0f}".format(yesterday_premium)
+    fom_yesterday_premium = "Ksh. {:,.0f}".format(yesterday)
     yesterday_receipts = most_recent_date[most_recent_date['RECEIPTS'] > 0]
     yesterday_receipts_total = yesterday_receipts['RECEIPTS'].sum()
     fom_yesterday_receipts = "Ksh. {:,.0f}".format(yesterday_receipts_total)
